3f4b5ce8abde03f9a7d42223c66a9165217f1992
Sergei A Mamonov <mrqwer88@gmail.com>
2026-04-22 19:02:36 +0300

    add create_storage_default_dirs.sh for tests

---
+++ create_storage_default_dirs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p storage/mongo/data/
+touch storage/mongo/data/.gitkeep
+touch storage/.gitkeep
+
+mkdir -p storage/fastnetmon/logs/
+touch storage/fastnetmon/logs/.gitkeep
+touch storage/fastnetmon/.gitkeep
+
+mkdir -p storage/fastnetmon_web_api/logs/
+touch storage/fastnetmon_web_api/logs/.gitkeep
+
+mkdir -p storage/clickhouse/data/
+touch storage/clickhouse/data/.gitkeep
+
+mkdir -p storage/clickhouse/logs/
+touch storage/clickhouse/logs/.gitkeep
+
+mkdir -p storage/grafana/data/
+touch storage/grafana/data/.gitkeep
+
+
+

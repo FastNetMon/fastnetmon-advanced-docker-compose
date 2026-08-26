c3e3fcbfc62e47d33fba9353e69f4c22efb9365a
Sergei A Mamonov <mrqwer88@gmail.com>
2026-08-26 19:59:50 +0300

    move init scripts to dir scripts, port them to sh

---
+++ scripts/init_secrets.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -eu
+
+PROJECT_DIR="$(pwd)"
+
+if [ ! -f "./docker-compose.yml" ]; then
+ echo "Please run this script from the repository root." >&2
+    exit 1
+fi
+
+SECRETS_NAMES="
+mongo_password
+clickhouse_password
+grafana_admin_password
+clickhouse_grafana_password
+web_api_admin_password"
+
+#for secret in "${SECRETS_NAMES[@]}"
+for secret in $SECRETS_NAMES;do
+    echo "Generate and write $secret"
+    if [ -f "${PROJECT_DIR}/secrets/$secret" ]; then
+        echo "$secret secret file already exist\n"
+    else
+        password=`printf "%s" $(pwgen 16 1)`
+        if [ "$secret" = "web_api_admin_password" ]; then
+            password=`printf "_%s" $(pwgen 15 1)`
+        fi
+        echo -n $password > ${PROJECT_DIR}/secrets/$secret
+    fi
+done
+
+echo "Done"

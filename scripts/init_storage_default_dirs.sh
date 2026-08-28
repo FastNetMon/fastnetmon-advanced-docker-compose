fbadce61149be1ba2a7fea8484e162bd9c6ccf85
Sergei A Mamonov <mrqwer88@gmail.com>
2026-08-28 14:27:43 +0300

    minor fixes to init_storage_default_dirs.sh

--- scripts/init_storage_default_dirs.sh
+++ scripts/init_storage_default_dirs.sh
@@ -8,25 +8,25 @@ if [ ! -f "./docker-compose.yml" ]; then
     exit 1
 fi
 
-mkdir -p ${PROJECT_DIR}/storage/mongo/data/
-touch ${PROJECT_DIR}/storage/mongo/data/.gitkeep
-touch ${PROJECT_DIR}/storage/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/mongo/data/"
+touch "${PROJECT_DIR}/storage/mongo/data/.gitkeep"
+touch "${PROJECT_DIR}/storage/.gitkeep"
 
-mkdir -p ${PROJECT_DIR}/storage/fastnetmon/logs/
-touch ${PROJECT_DIR}/storage/fastnetmon/logs/.gitkeep
-touch ${PROJECT_DIR}/storage/fastnetmon/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/fastnetmon/logs/"
+touch "${PROJECT_DIR}/storage/fastnetmon/logs/.gitkeep"
+touch "${PROJECT_DIR}/storage/fastnetmon/.gitkeep"
 
-mkdir -p ${PROJECT_DIR}/storage/fastnetmon_web_api/logs/
-touch ${PROJECT_DIR}/storage/fastnetmon_web_api/logs/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/fastnetmon_web_api/logs/"
+touch "${PROJECT_DIR}/storage/fastnetmon_web_api/logs/.gitkeep"
 
-mkdir -p ${PROJECT_DIR}/storage/clickhouse/data/
-touch ${PROJECT_DIR}/storage/clickhouse/data/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/clickhouse/data/"
+touch "${PROJECT_DIR}/storage/clickhouse/data/.gitkeep"
 
-mkdir -p ${PROJECT_DIR}/storage/clickhouse/logs/
-touch ${PROJECT_DIR}/storage/clickhouse/logs/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/clickhouse/logs/"
+touch "${PROJECT_DIR}/storage/clickhouse/logs/.gitkeep"
 
-mkdir -p ${PROJECT_DIR}/storage/grafana/data/
-touch ${PROJECT_DIR}/storage/grafana/data/.gitkeep
+mkdir -p "${PROJECT_DIR}/storage/grafana/data/"
+touch "${PROJECT_DIR}/storage/grafana/data/.gitkeep"
 
 
 
